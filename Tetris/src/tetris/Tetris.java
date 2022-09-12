8f662ceb72f75a448624f99349d8bfc39c301472
CinthyaJG9 <cjimenezg2000@alumno.ipn.mx>
2022-09-11 21:52:50 -0500

    Tetris

---
+++ Tetris/src/tetris/Tetris.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tetris;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+/**
+ *
+ * @author cferg
+ */
+public class Tetris extends JFrame {
+
+    private JLabel statusbar;
+
+    public Tetris() {
+
+        initUI();
+    }
+
+    private void initUI() {
+
+        statusbar = new JLabel(" 0");
+        add(statusbar, BorderLayout.SOUTH);
+
+        Tablero board = new Tablero(this);
+        add(board);
+        board.start();
+
+        setTitle("Tetris");
+        setSize(200, 400);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+    }
+
+    JLabel getStatusBar() {
+
+        return statusbar;
+    }
+
+    public static void main(String[] args) {
+
+        EventQueue.invokeLater(() -> {
+
+            Tetris game = new Tetris();
+            game.setVisible(true);
+        });
+    }
+}
